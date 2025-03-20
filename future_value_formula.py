f1faabfa021d1e76c05e81b21ed126a3922d2d94
Ricky <raflemin@gmail.com>
2025-03-20 14:32:02 -0700

    present value lump sum

--- future_value_formula.py
+++ future_value_formula.py
@@ -1,11 +1,20 @@
-
-
-def fv_eq(pv:float, n: float, r: float) -> float:
+def fv_lump_sum(pv:float, n: float, r: float) -> float:
     '''
     Returns the future value of a lump sum given:
     pv = present value
     n = number of periods
     r = interest rate
     '''
-    fv = pv * (1 + r)**n
+    fv = pv * ((1 + r)**n)
     return fv
+
+def pv_lump_sum(fv:float, n:float, r:float) -> float:
+    '''
+    Returns the present value of a lump sum given:
+    fv = future value
+    n = number of periods
+    r = interest rate
+    '''
+    pv = fv / ((1+r)**n)
+    return pv
+
