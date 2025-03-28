59d19c89904aa0b09cb1bd6870f02cb09bb6757a
Ricky <raflemin@gmail.com>
2025-03-28 10:25:11 -0700

    create and return dict

--- amortization_table.py
+++ amortization_table.py
@@ -3,11 +3,16 @@ from future_value_formula import pmt_pv_ordinary_annuity
 
 def create_amor_dict(pv=float, n=float, r=float) -> dict:
     '''
-    Returns a dict to resemble an amortization table of a loan given:
+    Returns a dict of lists to resemble an amortization table of a loan given:
     pv = Present Value
     n = Number of Periods
     r = Interest rate
     '''
 
     pmt = pmt_pv_ordinary_annuity(pv, n, r)
-    
+    amor_dict = {}
+
+    for period in range(1,n + 1):
+        amor_dict[str(period)] = [period, pmt]
+
+    return pmt
