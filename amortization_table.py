efb3b2b0ce3d43621683903ef5b0b8b192d874b3
Ricky <raflemin@gmail.com>
2025-04-29 17:17:53 -0700

    add input fields for loan amount, periods, and rate

--- amortization_table.py
+++ amortization_table.py
@@ -17,7 +17,7 @@ def create_amor_dict(pv=float, n_period=float, rate=float) -> dict:
     for period in range(1,n_period + 1):
         interest = round(balance * rate,2)
         principal = round(pmt - interest,2)
-        ending_balance = pv_ordinary_annuity(pmt, n=n_period-period, r=rate)
+        ending_balance = round(pv_ordinary_annuity(pmt, n=n_period-period, r=rate),2)
         amor_dict[str(period)] = [period, balance, pmt, interest, principal, ending_balance]
         balance = ending_balance
 
@@ -25,7 +25,25 @@ def create_amor_dict(pv=float, n_period=float, rate=float) -> dict:
 
 st.write('Loan Calculator')
 
-calc = create_amor_dict(pv=100000, n_period=12, rate=0.05)
+loan_amount = st.number_input(label = 'Loan Amount',
+                              min_value = 0.1,
+                              value = 'min',
+                              step = .01,
+                              format ="%0.2f")
+
+num_period = st.number_input(label = 'Number of Periods',
+                             min_value = 1,
+                             value = 'min',
+                             step = 1)
+
+int_rate = st.number_input(label = 'Interest Rate',
+                           min_value = 0.01,
+                           max_value = 0.99,
+                           value = 'min',
+                           step = 0.01,
+                           format = "%0.2f")
+
+calc = create_amor_dict(pv=loan_amount, n_period=num_period, rate=int_rate)
 
 df = pd.DataFrame.from_dict(calc, orient='index', columns=
                             ['period', 'balance', 'payment', 'interest', 'principal', 'ending balance'])
